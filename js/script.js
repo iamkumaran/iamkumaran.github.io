a67881c3deaf4111c2983bff6ecc47563896741b
Muthu Kumaran <mkumaran@sapient.com>
2017-06-23 00:18:02 +0530

    vue.js - part 1

--- js/script.js
+++ js/script.js
@@ -7,7 +7,7 @@
     $('.page-scroll a').bind('click', function(event) {
         var $anchor = $(this);
         $('html, body').stop().animate({
-            scrollTop: ($($anchor.attr('href')).offset().top - 50)
+            scrollTop: ($($anchor.attr('href')).offset().top - 70)
         }, 1250, 'easeInOutExpo');
         event.preventDefault();
     });
